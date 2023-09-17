2198a957e499be2dd4f54fcf25a57c29faafa525
unknown <jasiridickson@gmail.com>
2023-09-17 17:01:31 -0500

    <First commit for single test (not working for unknown reasons>

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,13 +1,19 @@
 const input = require('readline-sync');
+const prompt = require("prompt-sync");
+
 
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = ``;
+
+
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+
+let question = `Who was the first woman in space? `;
+let correctAnswer = `Sally Ride`;
+let candidateAnswer = ``;
 
 
 //TODO: Variables for Part 2
@@ -19,11 +25,16 @@ let candidateAnswers;
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
 
+  /*candidateName = input.question(`What is your name?`);
+  console.log(candidateName); This is code that does not work due to prompt/readline being noncompliant
+  I have replaced it with a placeholder and left it commented out*/
+
+  candidateName = `Joseph`
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+  candidateAnswer = input.question(question);
 
 }
 
@@ -31,7 +42,12 @@ function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-
+  if(candidateAnswer = correctAnswer){
+    console.log(`That's correct, good job!`);
+  }
+  else{
+    console.log(`Sorry, but that's incorrect.`);
+  }
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
@@ -42,7 +58,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+   console.log(`Hello ${candidateName}, it's time for your certification exam.`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
